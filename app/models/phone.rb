8d9cb03125c76b602445b33813bf53266eec780c
Aleperen Bozkurt <bozkurttalperen@gmail.com>
2017-09-05 12:31:50 +0300

    Add humanize method to phones

--- app/models/phone.rb
+++ app/models/phone.rb
@@ -1,4 +1,11 @@
 class Phone < ApplicationRecord
   belongs_to :contact
   validates :phone, presence: true
+
+  def humanize
+    phone = self.phone
+    if phone.size == 11 && phone[0] = "0"
+      phone[0..3] + " " + phone[4..6] + " " + phone[7..10]
+    end
+  end
 end
